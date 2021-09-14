ebbf639dd6d9ce84248a327f8375c358f57993d7
hanxuanyu <2252193204@qq.com>
2021-09-14 17:27:31 +0800

    商品服务层编写-创建类

---
+++ Main/src/main/java/com/hxuanyu/commodity/service/CommodityService.java
@@ -0,0 +1,31 @@
+package com.hxuanyu.commodity.service;
+
+import com.hxuanyu.commodity.beans.Commodity;
+import com.hxuanyu.commodity.dao.CommodityDao;
+import com.hxuanyu.commodity.enums.StatusCode;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 商品服务
+ *
+ * @author hxuanyu
+ */
+
+@Service
+public interface CommodityService {
+
+    /**
+     * 获取所有商品
+     * @return 商品列表
+     */
+    List<Commodity> getAllCommodity();
+
+    /**
+     * 根据id获取商品
+     * @param id 商品id
+     * @return 状态码
+     */
+    Commodity getCommodityById(int id);
+}
